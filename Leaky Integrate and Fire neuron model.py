c54a398526c2f2de69bf6fc6519b6c2eeea6f600
kumarnalin <nalin1131@gmail.com>
2026-06-11 15:40:45 +0530

    Commit changes to Github

---
+++ Leaky Integrate and Fire neuron model.py
@@ -0,0 +1,49 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# define variables
+t_max = 150e-3   # second
+dt = 1e-3        # second
+tau = 20e-3      # second
+el = -60e-3      # milivolt
+vr = -70e-3      # milivolt
+vth = -50e-3     # milivolt
+r = 100e6        # ohm
+i_mean = 25e-11  # ampere
+
+# Set random number generator
+np.random.seed(0)
+
+# Initialize step_end, n, t_range, v and i
+step_end = int(t_max / dt)
+n = 50
+t_range = np.linspace(0, t_max, num=step_end)
+v_n = el * np.ones([n, step_end])
+i = i_mean * (1 + 0.1 * (t_max / dt)**(0.5) * (2 * np.random.random([n, step_end]) - 1))
+
+# Loop for step_end - 1 steps
+for step in range(1, step_end):
+
+  # Compute v_n
+  v_n[:, step] = v_n[:, step - 1] + (dt / tau) * (el - v_n[:, step - 1] + r * i[:, step])
+
+# Compute sample mean (use np.mean)
+v_mean = np.mean(v_n, axis=0)
+
+# Compute sample standard deviation (use np.std)
+v_std = np.std(v_n, axis=0)
+
+# Plot figure
+plt.figure()
+plt.title('Multiple realizations of V_m')
+plt.xlabel('time (s)')
+plt.ylabel('V_m (V)')
+
+plt.plot(t_range, v_n.T, 'k', alpha=0.1)
+
+plt.plot(t_range, v_mean, 'C3', alpha=0.8, label='mean')
+plt.plot(t_range, v_mean+v_std, 'C9', alpha=0.8)
+plt.plot(t_range, v_mean-v_std, 'C9', alpha=0.8, label='std deviation')
+
+plt.legend()
+plt.show()
